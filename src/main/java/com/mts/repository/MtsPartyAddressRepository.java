b26950f89c237d0ad8229c19f0eee81a4faf9a3e
mrshi <mrshi@Shibam>
2024-08-22 23:38:05 +0530

    mail,contact,contactNumber getAddressByPartyId

--- src/main/java/com/mts/repository/MtsPartyAddressRepository.java
+++ src/main/java/com/mts/repository/MtsPartyAddressRepository.java
@@ -16,7 +16,8 @@ public interface MtsPartyAddressRepository extends JpaRepository<MtsPartyAddress
 			+ " pa.companyId = com.companyId and pm.mtsPartyMasterId = pa.mtsPartyMasterId", nativeQuery = true)
 	List<MtsPartyAddress> getAllPartyAddresses();
 
-	@Query(value = "select pa.mtsPartyAddressId, pa.addressCode, pa.details, pa.name from mts_party_address pa,"
+	@Query(value = "select pa.mtsPartyAddressId, pa.addressCode, pa.details, pa.name, pa.GSTN,"
+			+ " pa.emailAddress, pa.contactNumber from mts_party_address pa,"
 			+ " mts_party_master pm where pa.mtsPartyMasterId = :mtsPartyMasterId and "
 			+ "pa.mtsPartyMasterId = pm.mtsPartyMasterId", nativeQuery = true)
 	List<Map<String, Object>> getAddressByPartyId(@Param("mtsPartyMasterId") Long mtsPartyMasterId);
