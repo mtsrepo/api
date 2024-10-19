2e1273432ccb5790ae130d16f78f45aba11967c3
shibam007 <shibam.das@hepmade.com>
2024-10-20 03:14:44 +0530

    role in /validate

--- src/main/java/com/mts/repository/MtsUserRepository.java
+++ src/main/java/com/mts/repository/MtsUserRepository.java
@@ -11,7 +11,7 @@ import com.mts.entity.MtsUser;
 public interface MtsUserRepository extends JpaRepository<MtsUser, Long> {
 
 	@Query(value = "SELECT m.mtsUserName, m.emailAddress, m.mtsUserId, m.mtsUserCode, \r\n"
-			+ "     m.contactNumber, m.password, r.roleId, r.name FROM mts_user_ m LEFT JOIN \r\n"
+			+ "     m.contactNumber, m.password, r.roleId, r.role FROM mts_user_ m LEFT JOIN \r\n"
 			+ "       mts_user_role_mapping rm ON m.mtsUserId = rm.userId LEFT JOIN\r\n"
 			+ "       role_ r ON rm.roleId = r.roleId WHERE m.emailAddress = :idEmailContact \n"
 		    + " OR m.contactNumber = :idEmailContact OR m.mtsUserCode = :idEmailContact", nativeQuery = true)
