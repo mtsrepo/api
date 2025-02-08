059e80405a57c824af0e5242f4505ae894bf4b38
shibam007 <shibam.das@hepmade.com>
2025-02-08 12:39:29 +0530

    ...

---
+++ src/main/java/com/mts/dataObjects/EquiReq.java
@@ -0,0 +1,13 @@
+package com.mts.dataObjects;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import lombok.Data;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@Data
+public class EquiReq {
+	private String userId;
+	private String authToken;
+	private Long mtsEquipMasterId;
+}
