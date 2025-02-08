95fe3e21d046a2c9fcf565692fa4b665dc098a88
shibam007 <shibam.das@hepmade.com>
2025-02-08 15:28:01 +0530

    revert

--- src/main/java/com/mts/repository/MtsPartyAddressRepository.java
+++ src/main/java/com/mts/repository/MtsPartyAddressRepository.java
@@ -16,14 +16,14 @@ public interface MtsPartyAddressRepository extends JpaRepository<MtsPartyAddress
 			+ " pa.companyId = com.companyId and pm.mtsPartyMasterId = pa.mtsPartyMasterId", nativeQuery = true)
 	List<MtsPartyAddress> getAllPartyAddresses();
 
-//	@Query(value = "select pa.mtsPartyAddressId, pa.contactPerson, pa.designation, pa.addressCode, pa.addressLine1,"
-//			+ " pa.addressLine2, pa.locationAddressDesc, pa.GSTN, pa.emailAddress, pa.department, pa.pincode, pa.contactNumber from mts_party_address pa,"
-//			+ " mts_party_master pm where pa.mtsPartyMasterId = :mtsPartyMasterId and "
-//			+ "pa.mtsPartyMasterId = pm.mtsPartyMasterId", nativeQuery = true)
-	@Query(value = "select lm.mtsLocationMasterId, pa.contactPerson, pa.designation, pa.addressCode, pa.addressLine1,\r\n"
-			+ " pa.addressLine2, lm.description, pa.GSTN, pa.emailAddress, pa.department, pa.pincode, pa.contactNumber from mts_party_address pa,\r\n"
-			+ " mts_party_master pm, mts_location_master lm where pa.mtsPartyMasterId = :mtsPartyMasterId and \r\n"
-			+ " pa.mtsPartyMasterId = pm.mtsPartyMasterId and lm.mtsLocationMasterId = pa.mtsPartyAddressId", nativeQuery = true)
+	@Query(value = "select pa.mtsPartyAddressId, pa.contactPerson, pa.designation, pa.addressCode, pa.addressLine1,"
+			+ " pa.addressLine2, pa.locationAddressDesc, pa.GSTN, pa.emailAddress, pa.department, pa.pincode, pa.contactNumber from mts_party_address pa,"
+			+ " mts_party_master pm where pa.mtsPartyMasterId = :mtsPartyMasterId and "
+			+ "pa.mtsPartyMasterId = pm.mtsPartyMasterId", nativeQuery = true)
+//	@Query(value = "select lm.mtsLocationMasterId, pa.contactPerson, pa.designation, pa.addressCode, pa.addressLine1,\r\n"
+//			+ " pa.addressLine2, lm.description, pa.GSTN, pa.emailAddress, pa.department, pa.pincode, pa.contactNumber from mts_party_address pa,\r\n"
+//			+ " mts_party_master pm, mts_location_master lm where pa.mtsPartyMasterId = :mtsPartyMasterId and \r\n"
+//			+ " pa.mtsPartyMasterId = pm.mtsPartyMasterId and lm.mtsPartyAddressId = pa.mtsPartyAddressId", nativeQuery = true)
 	List<Map<String, Object>> getAddressByPartyId(@Param("mtsPartyMasterId") Long mtsPartyMasterId);
 
 	Optional<MtsPartyAddress> findByMtsPartyAddressId(Long mtsPartyAddressId);
